699a2e34cd4db5cea182b98fed16d954eb9bfa3d
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-01-14 19:14:41 +0530

    changed the ssr to ssg

---
+++ 2-fetching/src/app/users/[userId]/not-found.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center flex-col">
+      <p className=" text-4xl">USER NHI MILA BHAI !</p>
+      <p>URL check kr, pkka tune kuch galat dala hai</p>
+    </div>
+  );
+}
--- 2-fetching/src/app/users/[userId]/page.tsx
+++ 2-fetching/src/app/users/[userId]/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React, { Suspense } from "react";
 import UserPosts from "./components/UserPosts";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 interface UserParamsType {
   params: {
@@ -14,6 +15,12 @@ export async function generateMetadata({
 }: UserParamsType): Promise<Metadata> {
   const response: Promise<ResponseUsersType> = getUserData(userId);
   const responseJson = await response;
+
+  if (!responseJson) {
+    return {
+      title: "User not found!",
+    };
+  }
   return {
     title: responseJson.name,
     description: responseJson.id,
@@ -25,14 +32,21 @@ async function getUserData(userId: string) {
     `https://6592c197bb12970719900132.mockapi.io/api/users/${userId}`
   );
   if (!response.ok) {
-    throw new Error("Error fetching user data");
+    return undefined;
   }
   return await response.json();
 }
+//
+//{cache:"force-store"} - default -> caches the dynamic requests as well, but if the data is being constantly updates, its not the best option
+
+//{cache:"no-store"} -> always dynamic data
+
+//{ next: { revalidate: 60 } } -> ISR (revalidating after every 60 seconds or whatever number given)
 
 async function getUserPosts(userId: string) {
   const response = await fetch(
-    `https://6592c197bb12970719900132.mockapi.io/api/users/${userId}/posts`
+    `https://6592c197bb12970719900132.mockapi.io/api/users/${userId}/posts`,
+    { next: { revalidate: 60 } }
   );
   if (!response.ok) {
     throw new Error("Error fetching user data");
@@ -43,6 +57,9 @@ async function getUserPosts(userId: string) {
 export default async function UserPage({ params: { userId } }: UserParamsType) {
   const userData: ResponseUsersType = await getUserData(userId);
   const postsPromise: Promise<ResponsePostsType[]> = getUserPosts(userId);
+
+  if (!userData) notFound();
+
   return (
     <div>
       <p>{userData.name}</p>
@@ -59,4 +76,17 @@ export default async function UserPage({ params: { userId } }: UserParamsType) {
       </Suspense>
     </div>
   );
+}
+
+export async function generateStaticParams() {
+  const response = await fetch(
+    "https://6592c197bb12970719900132.mockapi.io/api/users"
+  );
+  if (!response.ok) {
+    return undefined;
+  }
+  const users: ResponseUsersType[] = await response.json();
+  return users.map((user) => ({
+    userId: user.id.toString(),
+  }));
 }
