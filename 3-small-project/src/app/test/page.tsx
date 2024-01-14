3e50940eb4b024d8531ce7ec8760ce25ca6d6c7e
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-01-14 22:02:43 +0530

    added shadcn

---
+++ 3-small-project/src/app/test/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { Download } from "lucide-react";
+
+export default function page() {
+  return (
+    <div>
+      <Button disabled>
+      <Download strokeWidth={3} />
+        Please wait
+      </Button>
+    </div>
+  );
+}
