c7d4ca548cc658927609050edc3db534fddf6d45
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-01-16 22:06:50 +0530

    minor changes

--- 3-small-project/src/components/Navbar.tsx
+++ 3-small-project/src/components/Navbar.tsx
@@ -1,14 +1,28 @@
+"use client"
+
 import Link from "next/link";
 import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "./ui/button";
+import { Search } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <header className=" border-b-2 border-slate-900 p-5">
+    <header className=" border-b-2 border-slate-900 p-5 flex justify-between items-center">
       <h1>
         <Link className="font-bold" href="/">
           Wikirocket
         </Link>
       </h1>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="__search flex gap-2"
+      >
+        <Input className=" bg-slate-800" placeholder="Search Text..." />
+        <Button>
+          <Search />
+        </Button>
+      </form>
     </header>
   );
 }
