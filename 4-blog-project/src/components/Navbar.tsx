b6f801b7b84593d2b76aa85c082347626bf3176d
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-01-21 20:49:21 +0530

    added blog mini project

---
+++ 4-blog-project/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <header className="w-full h-[60px] flex justify-center items-center font-bold border-b-[1px] border-slate-800">
+      Next Blog App
+    </header>
+  );
+}
